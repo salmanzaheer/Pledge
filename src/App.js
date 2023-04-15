7d5637423e237d8d88781ffac0f9066c99269922
Salman Zaheer <szahee2@students.towson.edu>
2023-04-15 19:18:59 -0400

    fixed hamburger menu control. Ready to start adding authentication and db

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ let menu = document.querySelector('#menu-icon');
 let navlist = document.querySelector('.navlist');
 
 menu.onclick = () => {
-  menu.classList.toggle('bx-menu');
+  // menu.classList.toggle('bx-menu');
   navlist.classList.toggle('open');
 }
 
