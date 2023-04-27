e3e88962f8a06b5b18b082b187f6334737fd2bdd
chris <cy.chrisyue@gmail.com>
2023-04-27 00:32:19 -0400

    testing email and google signup

---
+++ src/components/auth.js
@@ -0,0 +1,52 @@
+import { auth, googleProvider } from "../config/firebase";
+import {
+  createUserWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
+import { useState } from "react";
+
+export const Auth = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const signUp = async () => {
+    try {
+      await createUserWithEmailAndPassword(auth, email, password);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const signInWithGoogle = async () => {
+    try {
+      await signInWithPopup(auth, googleProvider);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const logout = async () => {
+    try {
+      await signOut(auth);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  return (
+    <div>
+      <input placeholder="email" onChange={(e) => setEmail(e.target.value)} />
+      <input
+        placeholder="password"
+        type="password"
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button onClick={signUp}>Sign Up</button>
+
+      <button onClick={signInWithGoogle}>Sign In With Google</button>
+
+      <button onClick={logout}>Logout</button>
+    </div>
+  );
+};
