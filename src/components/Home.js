0a8db275ed47ed8b25cad780c73f9e35bf8767ed
chris <cy.chrisyue@gmail.com>
2023-04-27 19:39:47 -0400

    added to the homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,3 +1,21 @@
+import { Link } from "react-router-dom";
+
 export const Home = () => {
-  return <h1>Home Page</h1>;
+  return (
+    <div>
+      <section class="hero">
+        <div class="hero-text">
+          <h5>Fast, safe, convenient payments!</h5>
+          <p>Pay. Get paid. You set your own terms. Ready to make a Pledge?</p>
+          <Link to="" class="button auth-btn">
+            Get Started
+          </Link>
+        </div>
+
+        <div class="hero-img">
+          <img src="./images/pledgePhone.png" alt="" />
+        </div>
+      </section>
+    </div>
+  );
 };
