63ab8a464e4df3650fc10d6733687072071ec5af
chris <cy.chrisyue@gmail.com>
2023-05-01 13:12:57 -0400

    fixed typo

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Auth } from "./components/auth";
 import { Home } from "./components/Home";
-import { Navbar } from "./components/Home";
+import { Navbar } from "./components/Navbar";
 
 function App() {
   return (
