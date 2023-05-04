674895630b4c99fc1d4fd8df55cd985749f4bfb8
Salman Zaheer <szahee2@students.towson.edu>
2023-05-04 00:03:59 -0400

    cleaned up code and created dashboard

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { MDBCol, MDBContainer, MDBRow, MDBCard, MDBCardText, MDBCardBody, MDBCardImage, MDBTypography, MDBIcon } from 'mdb-react-ui-kit';
+
 
 export const Profile = () => {
  
